25263039de4a874645b9a9e8ac04325bc1e0a62f
Oderinde Emmanuel Oluwatobiloba <oderinde.oluwatobiloba@gmail.com>
2023-02-23 17:47:08 +0100

    --

--- src/pages/Dashboard/project-dashboard/index.js
+++ src/pages/Dashboard/project-dashboard/index.js
@@ -62,6 +62,7 @@ import DesignChangeOrderForm from "../../forms/Budget/design-change-form";
 import MainAffidavit from "../../forms/E-589C Affidavit Of Capital Improvement/MainAffidavit";
 import DPSShortFormEngineerForm from "../../forms/DPS Short Form Engineer";
 import OwnerAndContractorForm from "../../forms/Contract/Owner and Contractor Agreement Form (more $500,000)/OwnerAndContractor";
+<<<<<<< HEAD
 
 import ChangeOrderDirectiveForm from "../../forms/Budget/Change Order Directive";
 import ChangeOrderForm from "../../forms/Budget/Change order form/ChangeOrderForm";
@@ -69,6 +70,8 @@ import DesignChangeOrderForm from "../../forms/Budget/design-change-form";
 import MainAffidavit from "../../forms/E-589C Affidavit Of Capital Improvement/MainAffidavit";
 import DPSShortFormEngineerForm from "../../forms/DPS Short Form Engineer";
 import OwnerAndContractorForm from "../../forms/Contract/Owner and Contractor Agreement Form (more $500,000)/OwnerAndContractor";
+=======
+>>>>>>> 5c03d66ecf01b6a6af73dc171f90f40981506202
 
 const ProjectDashboard = () => {
   const response = useFetchSingleProjectQuery(getId());
@@ -371,6 +374,7 @@ const ProjectDashboard = () => {
                 {/* AWARDEEE SPACE */}
                 <RenderAwardee />
 
+<<<<<<< HEAD
                 <div className="mb-5">
                   <div className="border-b border-b-gray-100 pb-2 text-[#2f5461] font-bold">
                     Project Manager
@@ -422,6 +426,59 @@ const ProjectDashboard = () => {
       <OwnerAndContractorForm id={documentsID} />
     </section>
   );
+=======
+								<div className="mb-5">
+									<div className="border-b border-b-gray-100 pb-2 text-[#2f5461] font-bold">
+										Project Manager
+									</div>
+									<div className="flex items-center gap-2 mt-4">
+										<div className="w-6 h-6 rounded-full">
+											{/* <img className="w-full" src={!projectDetails} alt="user" /> */}
+										</div>
+										<span className="text-xs text-[#2f5461]">
+											{!projectDetails || !projectDetails.project_manager
+												? ""
+												: projectDetails?.project_manager.first_name +
+												  " " +
+												  projectDetails?.project_manager.last_name}
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+			<Lunsford id={documentsID} />
+			<PunchList id={documentsID} />
+			<NoticeOfIntentConsultant id={documentsID} />
+			<CertificateOfSubstantial id={documentsID} />
+			<NoticeToProceed id={documentsID} />
+			<ProjectCloseoutCheckList id={documentsID} />
+			<NoticeOfAwardConsultant id={documentsID} />
+			<AdvertisementBid id={documentsID} />
+			<CapitalProjectForm id={documentsID} />
+			<CCPRequisitionForm id={documentsID} />
+			<MWBEParticipation id={documentsID} />
+			<Esser id={documentsID} />
+			<EsserPM id={documentsID} />
+			<Lechase id={documentsID} />
+			<TechService id={documentsID} />
+			<NoticeOfAwardContrator id={documentsID} />
+			<DeterminationOFLowestBidder id={documentsID} />
+			<OwnerContractorManagementForm id={documentsID} />
+			<Bids id={documentsID} />
+			<PFForProjects id={documentsID} />
+			<ShortSmallFormDesignForm id={documentsID} />
+			<ChangeOrderDirectiveForm id={documentsID} />
+			<ChangeOrderForm id={documentsID} />
+			<DesignChangeOrderForm id={documentsID} />
+			<MainAffidavit id={documentsID} />
+			<DPSShortFormEngineerForm id={documentsID} />
+			<OwnerAndContractorForm id={documentsID} />
+		</section>
+	);
+>>>>>>> 5c03d66ecf01b6a6af73dc171f90f40981506202
 };
 
 export default ProjectDashboard;
