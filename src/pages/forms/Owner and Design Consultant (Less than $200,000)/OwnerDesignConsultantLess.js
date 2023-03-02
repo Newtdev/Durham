1657e10abb82c83cf565c0324e5c47b27c8b3868
oderinde300 <oderinde.oluwatobiloba@gmail.com>
2023-03-02 14:54:32 +0100

    owner design less updated

--- src/pages/forms/Owner and Design Consultant (Less than $200,000)/OwnerDesignConsultantLess.js
+++ src/pages/forms/Owner and Design Consultant (Less than $200,000)/OwnerDesignConsultantLess.js
@@ -1,79 +1,99 @@
+import { useEffect } from "react";
 import { useFormik } from "formik";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { useFillProjectDocumentMutation } from "../../../features/services/api";
 import { OwnerDesignConsultantLessSlug } from "../../../shared-component/slug";
+// import { OwnerDesignConsultantLessSchema } from "../../../yup";
 import { ModalOverlay } from "../../../ui";
 import { project_document_id } from "../../Dashboard/project-dashboard/ReducerSlice";
 import { modal, saveFormField } from "../reducer";
 import FormOne from "./FormOne";
 import FormTwo from "./FormTwo";
+import { nextStep, page } from "./reducer";
 import Preview from "./Preview";
 import { setResult } from "../../../shared-component";
-import { nextStep, page } from "./reducer";
+import { UseFetchFilledFormDetails } from "../../../hooks/useFetchFilled";
 
-const OwnerDesignConsultantLessForm = ({ id }) => {
-	const dispatch = useDispatch();
-	const pages = useSelector(page);
-	const show = useSelector(modal);
+const OwnerDesignConsultantLessForm = ({ id, filled }) => {
+  const dispatch = useDispatch();
+  const pages = useSelector(page);
+  const show = useSelector(modal);
 
-	const formID = useSelector(project_document_id);
+  const formID = useSelector(project_document_id);
 
-	const [fillProjectDocument, { isLoading }] = useFillProjectDocumentMutation();
+  const [fillProjectDocument, { isLoading }] = useFillProjectDocumentMutation();
+  const [a] = UseFetchFilledFormDetails(formID);
 
-	const HandleSubmit = async (values) => {
-		const response = await fillProjectDocument({
-			project_document_id: formID,
-			form_fields: setResult(values),
-		});
-		if (response) {
-			if (response?.error) {
-				toast.error(response?.message, {
-					position: toast.POSITION.TOP_CENTER,
-				});
-			} else {
-				dispatch(nextStep(3));
-			}
-		}
-	};
+  const HandleSubmit = async (values) => {
+    // const param = Object.keys(values);
+    // const val = Object.values(values);
 
-	// const show = useSelector(modal);
-	const formik = useFormik({
-		initialValues: {
-			agreementDate: "",
-			sustainaibilityDate: "",
-			financialOfficerSignDate: "",
-			notarizedDate: "",
-			sexualOffenderOption: false,
-		},
-		// validationSchema: OwnerDesignConsultantLessSchema,
-		onSubmit: (values) => {
-			if (pages === 1) {
-				// dispatch(saveFormField(values))
-				console.log(values);
+    // console.log("values: ", values);
+    // console.log("pages: ", pages);
 
-				dispatch(nextStep(3));
-				// HandleSubmit(values);
-			} else if (pages === 2) {
-				dispatch(saveFormField(values));
-				HandleSubmit(values);
-			}
-		},
-	});
+    const response = await fillProjectDocument({
+      project_document_id: formID,
+      form_fields: setResult(values),
+    });
+    if (response) {
+      if (response?.error) {
+        toast.error(response?.message, {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      } else {
+        dispatch(nextStep(3));
+      }
+    }
+  };
 
-	//return <ModalOverlay show={show} />
-	const formProps = {
-		...formik,
-		isLoading,
-	};
+  // const show = useSelector(modal);
+  const formik = useFormik({
+    initialValues: {
+      agreementDate: new Date(),
+      sustainaibilityDate: new Date(),
+      financialOfficerSignDate: new Date(),
+      notarizedDate: new Date(),
+      sexualOffenderOption: "",
+    },
+    // validationSchema: OwnerDesignConsultantLessSchema,
+    onSubmit: (values) => {
+      if (pages === 1) {
+        dispatch(saveFormField(values));
+        dispatch(nextStep(2));
+        // HandleSubmit(values);
+      } else if (pages === 2) {
+        console.log(values);
+        dispatch(saveFormField(values));
+        HandleSubmit(values);
+      }
+    },
+  });
 
-	return (
-		<ModalOverlay show={id === OwnerDesignConsultantLessSlug && show}>
-			{pages === 1 && <FormOne {...formik} />}
-			{pages === 2 && <FormTwo {...formProps} />}
-			{pages === 3 && <Preview />}
-		</ModalOverlay>
-	);
+  useEffect(() => {
+    if (!a?.data?.form_fields) {
+      return;
+    }
+    // setFieldValue('')
+    formik.setValues({
+      // ...a?.data?.form_fields,
+      // type: a?.data?.form_fields?.type,
+      // addressCopy: a?.data?.form_fields?.addressCopy,
+      sexualOffenderOption: a?.data?.form_fields?.sexualOffenderOption,
+    });
+  }, [a]);
+
+  //return <ModalOverlay show={show} />
+  const props = { ...formik, isLoading };
+
+  return (
+    <ModalOverlay show={id === OwnerDesignConsultantLessSlug && show}>
+      {filled ? <Preview {...formik} /> : null}
+      {pages === 1 && !filled ? <FormOne {...formik} /> : null}
+      {pages === 2 && !filled ? <FormTwo {...props} /> : null}
+      {pages === 3 && <Preview {...formik} />}
+    </ModalOverlay>
+  );
 };
 
 export default OwnerDesignConsultantLessForm;
