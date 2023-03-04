1cb62dd1202c53dc5e847f6dd58167b526588ba4
oderinde300 <oderinde.oluwatobiloba@gmail.com>
2023-03-04 09:45:36 +0100

    owner design less page number

--- .history/src/pages/forms/Owner and Design Consultant (Less than $200,000)/Preview Pages/Page12_20230302175231.jsx
+++ .history/src/pages/forms/Owner and Design Consultant (Less than $200,000)/Preview Pages/Page12_20230302175231.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "./OwnerLessPages.css";
 
-const Page11 = () => {
+const Page12 = () => {
   return (
     <div style={{ margin: "1in", paddingTop: "6rem", height: "10in" }}>
       <p class="p51">
-        <span class="p7">Exhibit A &ndash; Proposal and Scope of Services</span>
+        <span class="p7">Exhibit B &ndash; Hourly Rates</span>
       </p>
 
       <div
         className="h-[55rem] mb-6 flex items-end"
-        style={{ fontFamily: "Arial", fontSize: "10pt" }}
+        style={{ fontFamily: "Arial" }}
       >
         <div>
           <p className="text-[10px]"></p>
           <p className="text-[10px]"></p>
         </div>
         <div className="mx-auto">
-          <p>11</p>
+          <p>12</p>
         </div>
       </div>
     </div>
   );
 };
 
-export default Page11;
+export default Page12;
